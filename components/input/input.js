1e5474e344c7d10ebde0d2d84b6366964905700f
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2022-02-04 22:53:49 +0530

    feat: added input & image components

---
+++ components/input/input.js
@@ -0,0 +1,12 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+const errMsg = document.querySelector("#errMsg");
+
+togglePassword.addEventListener("click", function () {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+
+  // toggle the icon
+  this.classList.toggle("fa-eye-slash");
+});
