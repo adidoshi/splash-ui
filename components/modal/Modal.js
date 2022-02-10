e1d242607947419f07b6507e15fe75bd0e9e8387
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2022-02-11 01:06:29 +0530

    fix: modal component
    body overflow hidden when modal is open.

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -5,12 +5,15 @@ const modal = document.querySelector(".modal-container");
 
 openModal.addEventListener("click", () => {
   modal.style.display = "flex";
+  document.body.style.overflow = "hidden";
 });
 
 dismiss.addEventListener("click", () => {
   modal.style.display = "none";
+  document.body.style.overflow = "visible";
 });
 
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
+  document.body.style.overflow = "visible";
 });
